5e2b09d9c49e78a00a554046c0d9c9375c154eaf
Siddhant <siddhantrsen73938@gmail.com>
2024-12-13 20:07:10 +0530

    Clearing out about page code for a while.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,18 +1,19 @@
-import About2 from "../Components/About/About2"
-import About3 from "../Components/About/About3"
-import AboutHero from "../Components/About/AboutHero"
-import AboutHero2 from "../Components/About/AboutHero2"
-import Team from "../Components/About/Team"
+// import About2 from "../Components/About/About2"
+// import About3 from "../Components/About/About3"
+// import AboutHero from "../Components/About/AboutHero"
+// import AboutHero2 from "../Components/About/AboutHero2"
+// import Team from "../Components/About/Team"
 
 
 const About = () => {
   return (
     <section className='min-h-screen flex items-center flex-col'>
-      <About2 />
+      {/* <About2 />
       <AboutHero />
       <AboutHero2 />
       <Team />
-      <About3 />
+      <About3 /> */}
+      about
     </section>
   )
 }
